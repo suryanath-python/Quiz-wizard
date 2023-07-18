f8b9f0e8d351dd79888430776375d05e6023052d
suryanath-python <69254475+suryanath-python@users.noreply.github.com>
2023-07-18 19:10:13 +0530

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -1,6 +1,7 @@
 from colorama import init
 from termcolor import colored
 import time
+import playmaths
 
 #fcn for timedelay messages
 def time_msg(s,clr='cyan'):
@@ -27,4 +28,16 @@ time_msg('''
 Welcome users, this is a python program that lets  you create and play Quizzes
 Press[1] for creating a quiz                     Press[2] for attending a quiz                   
 ''')
-input()
+print()
+g = int(input(''))
+
+
+if g==2:
+    print()
+    time_msg('Enter the subject:    [0] Maths          [1] Physics          [2] 2Chemistry')
+    print()
+
+    sub=int(input(''))
+    print()
+    if sub==0:
+        quiz=playmaths.get_quiz()
