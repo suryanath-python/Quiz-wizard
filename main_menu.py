537c02bc69bffc59fcdcfd7ea786cfb4de3800fc
suryanath-python <69254475+suryanath-python@users.noreply.github.com>
2023-09-29 22:30:14 +0530

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -2,14 +2,17 @@ from colorama import init
 from termcolor import colored
 import time
 import playmaths
+import scores
+init()
 
+global g
 #fcn for timedelay messages
 def time_msg(s,clr='cyan'):
     for i in s:
         print(colored(i,clr),end='',flush=True)
         time.sleep(0.01)
+    return ' '
 
-init()
  
 print(colored(
     
@@ -24,20 +27,33 @@ print(colored(
                                  ░░░░░░ ░░   ░░░░░░░░   ░░░░░ ░░░░░░░░░░░         ░░░   ░░░      ░░░░░ ░░░░░░░░░░░ ░░░░░   ░░░░░ ░░░░░   ░░░░░ ░░░░░░░░░░   
               ''', 'green'))
 
+
+name=input(time_msg('Enter your name :'))
+
 time_msg(''' 
 Welcome users, this is a python program that lets  you create and play Quizzes
-Press[1] for creating a quiz                     Press[2] for attending a quiz                   
+Press[1] for attending a quiz                     Press[2] Scoreboard                   
 ''')
 print()
 g = int(input(''))
 
 
-if g==2:
+if g==1:
+    time_msg('''
+  For every correct answer you will be awarded +1 mark
+  For every wrong answers -1 will be deducted
+
+    ''')
     print()
-    time_msg('Enter the subject:    [0] Maths          [1] Physics          [2] 2Chemistry')
+    time_msg('Enter the subject:    [0] Maths          [1] Physics          [2] Chemistry')
     print()
 
     sub=int(input(''))
     print()
     if sub==0:
-        quiz=playmaths.get_quiz()
+        score=playmaths.play_quiz()
+
+    scores.enter_name(name,score)
+
+if g==2:
+    scores.show()
